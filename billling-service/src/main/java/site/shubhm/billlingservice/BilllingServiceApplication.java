df16063eb8ba4b165e701e48d13a33b2453e5acc
Shubh1099 <Shubhammane434@gmail.com>
2026-01-28 01:13:08 +0530

    add billing service and grpc server.

---
+++ billling-service/src/main/java/site/shubhm/billlingservice/BilllingServiceApplication.java
@@ -0,0 +1,13 @@
+package site.shubhm.billlingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilllingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilllingServiceApplication.class, args);
+    }
+
+}
